9f7468ec6d5d0c5ea3100d18b34c6d758e1e4718
MUHAMMEDFAISALKC <87296701+MUHAMMEDFAISALKC@users.noreply.github.com>
2021-10-20 10:35:49 +0530

    Create channels.js

---
+++ js/channels.js
@@ -0,0 +1,42 @@
+const mockChannels= [
+    {
+    id: "000001",
+    name: "MeetUp",
+    favorite: true,
+    messages: [],
+    latestMessage: "17:24"
+    },
+    {
+    id : "000002",
+    name : "Octoberfest",
+    favorite : true,
+    messages: [],
+    latestMessage: "18:24"
+    },
+    {
+    id : "000003",
+    name : "WeatherChannel",
+    favorite : false,
+    messages: [],
+    latestMessage: "19:24"
+    },
+    {
+    id : "000004",
+    name : "SevenContinents",
+    favorite : false,
+    messages: [],
+    latestMessage: "20:24"
+    },
+    {
+    id : "000005",
+    name : "Dlc",
+    favorite : false,
+    messages: [],
+    latestMessage: "21:24"
+    }
+]
+
+
+
+
+
