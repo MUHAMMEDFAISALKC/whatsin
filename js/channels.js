716ce562fe15b3683f802f04de1022f414ec80a1
MUHAMMEDFAISALKC <87296701+MUHAMMEDFAISALKC@users.noreply.github.com>
2021-10-24 11:13:03 +0530

    Update channels.js

--- js/channels.js
+++ js/channels.js
@@ -1,39 +1,72 @@
+function Channel (name) {
+    this.id = ((Math.max(...channels.map(channel => channel.id))+1).toString()).padStart(6,0);
+    this.name = name;
+    this.favorite = false;
+    this.messages = [];
+}
+
+
 const mockChannels = [{
-    id: "000001",
-    name: "MeetUp",
-    favorite: true,
-    messages: [],
-    latestMessage: "11/12/2018 20:24:0"
+        id: '000001',
+        name: "MeetUp",
+        favorite: true,
+        messages: [],
+        latestMessage: latestMessageTime
     },
     {
-    id : "000002",
-    name : "Octoberfest",
-    favorite : true,
-    messages: [],
-    latestMessage: "9/12/2018 18:24:0"
+        id : '000002',
+        name : "Octoberfest",
+        favorite : true,
+        messages: [],
+        latestMessage: latestMessageTime
     },
     {
-    id : "000003",
-    name : "WeatherChannel",
-    favorite : false,
-    messages: [],
-    latestMessage: "12/12/2018 22:24:0"
+        id : '000003',
+        name : "WeatherChannel",
+        favorite : false,
+        messages: [],
+        latestMessage: latestMessageTime
     },
     {
-    id : "000004",
-    name : "SevenContinents",
-    favorite : false,
-    messages: [],
-    latestMessage: "No messages"
+        id : '000004',
+        name : "SevenContinents",
+        favorite : false,
+        messages: [],
+        latestMessage: latestMessageTime
     },
     {
-    id : "000005",
-    name : "Dlc",
-    favorite : false,
-    messages: [],
-    latestMessage: "No messages"
+        id : '000005',
+        name : "Dlc",
+        favorite : false,
+        messages: [],
+        latestMessage: latestMessageTime
+    },
+    {
+        id : '000006',
+        name : "pallimukku",
+        favorite : true,
+        messages: [],
+        latestMessage: latestMessageTime
     }
 ]
+
+
+function latestMessageTime () {
+    // if message exist, dispaly timestamp
+    if (!!this.messages.length) {
+        const latest = new Date(Math.max(...this.messages.map(message => message.createdOn)));
+        // if messages is from yesterday or older, display date , else display time
+        if (new Date().getDate() - latest.getDate() > 1) {
+            return latest.toLocaleDateString(browserLanguage, {year:"numeric", month:"numeric", day:"numeric"});
+        } else {
+            return latest.toLocaleTimeString(browserLanguage, {hour:"numeric", minute:"numeric",});
+        }
+    } else {
+        return 'No message';
+    }
+}
+
+
 
 
 
