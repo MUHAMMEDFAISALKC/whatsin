1225e2ff72c55e92ca4220478476499ab43cbc8b
MUHAMMEDFAISALKC <87296701+MUHAMMEDFAISALKC@users.noreply.github.com>
2021-11-13 08:27:54 +0530

    Update messages.js

--- js/messages.js
+++ js/messages.js
@@ -3,7 +3,10 @@ const mockMessages = [ {
     createdOn: new Date('10-01-2021 01:1:53'),
     channel: '000001',
     own: false,
-    text: "how are you guys!"
+    text: "how are you guys!",
+    yesterdayOrOlder() {
+        return new Date().getDate() - this.createdOn.getDate() >1
+    }
     },
 
     {
@@ -11,7 +14,10 @@ const mockMessages = [ {
     createdOn: new Date('10-02-2021 02:2:53'),
     channel: '000002',
     own: false,
-    text: "what about you!"
+    text: "what about you!",
+    yesterdayOrOlder() {
+        return new Date().getDate() - this.createdOn.getDate() >1
+    }
     },
 
     {
@@ -19,7 +25,10 @@ const mockMessages = [ {
     createdOn: new Date('10-03-2021 03:3:53'),
     channel: '000001',
     own: false,
-    text: "well guys!"
+    text: "well guys!",
+    yesterdayOrOlder() {
+        return new Date().getDate() - this.createdOn.getDate() >1
+    }
     },
 
     {
@@ -27,7 +36,10 @@ const mockMessages = [ {
     createdOn: new Date('10-04-2021 04:4:53'),
     channel: '000001',
     own: true,
-    text: "fine guys!"
+    text: "fine guys!",
+    yesterdayOrOlder() {
+        return new Date().getDate() - this.createdOn.getDate() >1
+    }
     },
 
     {
@@ -35,7 +47,10 @@ const mockMessages = [ {
     createdOn: new Date('10-05-2021 05:5:53'),
     channel: '000003',
     own: false,
-    text: "i think everyone feeling good!"
+    text: "i think everyone feeling good!",
+    yesterdayOrOlder() {
+        return new Date().getDate() - this.createdOn.getDate() >1
+    }
     }
 ];
 
